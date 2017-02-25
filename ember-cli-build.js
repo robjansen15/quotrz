7e3b4d5164e27c4de81e563fedd219c3c2d387b6
Robert Jansen <robjansen15@outlook.com>
2017-02-25 17:51:27 -0500

    Initial commit

---
+++ ember-cli-build.js
@@ -0,0 +1,16 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  app.import('https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+
+
+  return app.toTree();
+};
---
+++ tests/unit/views/nav-bar-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('view:nav-bar', 'Unit | View | nav bar');
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var view = this.subject();
+  assert.ok(view);
+});
